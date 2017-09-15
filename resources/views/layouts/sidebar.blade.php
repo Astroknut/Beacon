e3f5eb179d82eec4857b861b2432849ec51872b5
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 10:10:32 -0600

    builds sidebar template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="col-sm-3 offset-sm-1 sidebar">
+	<div class="sidebar-module sidebar-module-inset">
+		<h4>Preparedness Guides</h4>
+		<p>Insert disater preparation guides here</p>
+	</div>
+	<div class="sidebar-module">
+    <h4>Disaters</h4>
+    <ol class="list-unstyled">
+      <li><a href="#">Hurricane</a></li>
+      <li><a href="#">Tornado</a></li>
+      <li><a href="#">Fire</a></li>
+      <li><a href="#">Flood</a></li>
+      <li><a href="#">Earthquake</a></li>
+      <li><a href="#">Zombie Apocolypse</a></li>
+    </ol>
+  </div>
+</div>
