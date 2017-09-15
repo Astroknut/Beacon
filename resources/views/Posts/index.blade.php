2f07c6fa50901978c1063b5fb28f7f4a3debd44b
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 15:49:19 -0600

    formats new beacon button

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 	<div class="col-sm-8 feed-main">
-		<a href="/posts/create"><button>New Beacon</button></a>
+		<a href="/posts/create"><button class="btn btn-primary">New Beacon</button></a>
 		@foreach ($posts as $post)
 			@include('Posts.post')
 		@endforeach
