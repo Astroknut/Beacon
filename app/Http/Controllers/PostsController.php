c75f3ded89c11d349d9aec3dd9eaa66b35bd0610
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 12:38:47 -0600

    not auth issue test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,10 +7,10 @@ use App\Post;
 
 class PostsController extends Controller
 {
-    // public function __construct()
-    // {
-    // 	$this->middleware('auth');
-    // }
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
 
     public function index()
     {
