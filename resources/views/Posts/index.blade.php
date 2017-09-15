1c8905edaa870d1abf01a70658e1921b847069f7
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 15:31:09 -0600

    allows logo nav bag to /home

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -4,6 +4,7 @@
 @section('content')
 
 	<div class="col-sm-8 feed-main">
+		<a href="/posts/create"><button>New Beacon</button></a>
 		@foreach ($posts as $post)
 			@include('Posts.post')
 		@endforeach
