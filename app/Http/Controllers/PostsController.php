f112b8daa59f2cd6eddb63507a328b893a374742
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 13:22:34 -0600

    route test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -18,7 +18,7 @@ class PostsController extends Controller
 
         // return response("hello world");
 
-    	return View::make('posts.index', compact('posts'));
+    	return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post)
