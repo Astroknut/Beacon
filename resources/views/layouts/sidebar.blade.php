0350ff1ecd009c0fd0ec19538c725ed636bcef34
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 10:41:55 -0600

    adds live sidebar links

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,17 @@
 <div class="col-sm-3 offset-sm-1 sidebar">
 	<div class="sidebar-module sidebar-module-inset">
 		<h4>Preparedness Guides</h4>
-		<p>Insert disater preparation guides here</p>
+		<p>Red Cross Guides</p>
 	</div>
 	<div class="sidebar-module">
     <h4>Disaters</h4>
     <ol class="list-unstyled">
-      <li><a href="#">Hurricane</a></li>
-      <li><a href="#">Tornado</a></li>
-      <li><a href="#">Fire</a></li>
-      <li><a href="#">Flood</a></li>
-      <li><a href="#">Earthquake</a></li>
-      <li><a href="#">Zombie Apocolypse</a></li>
+      <li><a href="http://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/hurricane">Hurricane</a></li>
+      <li><a href="http://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/tornado">Tornado</a></li>
+      <li><a href="http://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/wildfire">Wildfire</a></li>
+      <li><a href="http://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/flood">Flood</a></li>
+      <li><a href="http://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/earthquake">Earthquake</a></li>
+      <li><a href="https://blogs.cdc.gov/publichealthmatters/2011/05/preparedness-101-zombie-apocalypse/">Zombie Apocolypse</a></li>
     </ol>
   </div>
 </div>
