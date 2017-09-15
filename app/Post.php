b19160b6b238c785d213ff70b10e27f7ca82cd1d
Astroknut <charlesbarrett1521@gmail.com>
2017-09-15 14:05:21 -0600

    updates Post model for comments addition

--- app/Post.php
+++ app/Post.php
@@ -8,9 +8,19 @@ class Post extends Model
 {
 
 	protected $guarded = [];
+
+	public function comments()
+	{
+		return $this->hasMany(Comment::class);
+	}
 	
     public function user()
     {
     	return $this->belongsTo(User::class);
+    }
+
+    public function addComment($body)
+    {
+    	$this->comments()->create(compact('body'));
     }
 }
