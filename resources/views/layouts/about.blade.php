13b6a41fa41161bacdba01e7070fdcf95eef0580
Astroknut <charlesbarrett1521@gmail.com>
2017-09-18 13:40:39 -0600

    adds about content

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -4,13 +4,13 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6">
+			<div class="col-sm-6 font">
 
 				<h1>About Beacon</h1>
 
 				<hr>
 
-				<p></p>
+				<h3>One of the most difficult parts of rebuilding after a natural disaster is connecting members of a community to help address the biggest needs in that area. Many people spend a large portion time trying to connect with friends and neighbors to figure out where they can help to begin the recovery process. Beacon seeks to give communities a platform to begin the recovery process. Beacon offers a live stream forum for community members in the same zip code to alert others of dangerous condition in the area, volunteer recovery efforts, or their own request for assistance after a disaster. </h3>
 
 			</div>
 		</div>
