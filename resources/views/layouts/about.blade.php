8fe81ec89d2ad5e786990c78d55e9d3e721383a5
Astroknut <charlesbarrett1521@gmail.com>
2017-09-18 16:52:15 -0600

    about content

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -11,9 +11,13 @@
 				<hr>
 
 				<h3>The most difficult aspect of recovery after a natural disaster is simply getting started. After an event like a hurricane, there may be countless people in need of assistance, and just as many willing to help but not knowing how they can. That's where Beacon come in.</h3>
+
 				<br>
+				
 				<h3>Beacon aims to provide communities with a platform to ask for assistance, or to offer their help. Each community has there own location-based activity feed, that allows others to respond to any request for aid, and help expedite the recovery process. Every time you log in, you will have direct access to your communities Broadcast Feed, and the ability to broadcast your own beacon.</h3>
 
+				<br>
+				
 				<a href="/home"><button class="btn btn-success">View Community Broadcast Feed</button></a>
 
 			</div>
